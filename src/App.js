6b2264ded4e46758b66079b8f95fac792908b4e2
NoumanSaeed <uzairshah866@yahoo.com>
2022-04-13 21:21:49 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Banner from './components/home/Banner';
+import Foundations from './components/home/Foundations';
+import Team from './components/home/Team';
+
+const App = () => {
+  return (
+    <>
+      <Banner />
+      {/* <hr /> */}
+      <Foundations />
+      {/* <hr /> */}
+      <Team />
+    </>
+  );
+};
+
+export default App;
